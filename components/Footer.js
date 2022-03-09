6673ae66ef14c82a0e9d94e19cde537cee16e3b7
Chintan <cgandhi4@gmail.com>
2022-03-09 15:02:08 +0530

    Modify footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,17 @@
 import styles from '@/styles/scss/Footer.module.scss';
 
-export default function Footer() {
+export default function Footer({ companyName }) {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; </p>
+      <p>
+        Copyright &copy;
+        {' '}
+        { companyName }
+      </p>
     </footer>
   );
 }
+
+Footer.defaultProps = {
+  companyName: 'Add Company name',
+};
